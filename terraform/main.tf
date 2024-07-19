35815976669638d06889cec06a59ff2d795732f3
amachlou <abderrahim.machlou@gmail.com>
2024-07-19 21:05:18 +0100

    configure storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,49 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.110.0"
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
     }
   }
 }
-  
+
 provider "azurerm" {
- features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
   location = "westeurope"
   name     = "gitops-ress-grp-222"
+}
+
+# *******************************************
+
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+# resource "azurerm_resource_group" "tfstate" {
+#   name     = "tfstate"
+#   location = "westeurope"
+# }
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate${random_string.resource_code.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    environment = "DEV"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
 }
