d90d887c2a1cd705d50585890eb41b96f0472cc8
amachlou <abderrahim.machlou@gmail.com>
2024-07-21 00:29:44 +0100

    Revert "Remote the remote backend"
    This reverts commit 7f5f595bfefc116087e0a62cffa644317f9cc1e8.

--- terraform/setup-backend-resources/main.tf
+++ terraform/setup-backend-resources/main.tf
@@ -9,13 +9,6 @@ terraform {
 #   backend "local" {
 #     path = "terraform.tfstate"
 #  }
-
-  backend "azurerm" {
-        resource_group_name  = var.resource_group_name
-        storage_account_name = var.storage_account_name
-        container_name       = var.storage_container_name
-        key                  = "terraform.tfstate"
-    }
 }
 
 provider "azurerm" {
